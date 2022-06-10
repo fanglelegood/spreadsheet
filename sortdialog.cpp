5369eb6f90548041a2c19acd73756f7d20577305
Fang Miao <fangmiaomin@163.com>
2022-06-10 17:07:12 +0800

    6.10公司

--- sortdialog.cpp
+++ sortdialog.cpp
@@ -35,3 +35,21 @@ void SortDialog::setColunmnRange(QChar first, QChar last)
     }
     
 }
+
+bool SpreadsheetCompare::operator()(const QStringList &row1,
+                                const QStringList &row2) const
+{
+    for (int i=0; i < KeyCount; ++i) {
+        int column = keys[i];
+        if (column != -1) {
+            if (row1[column] != row2[column]) {
+                if  (ascending[i]) {
+                    return row1[column] < row2[column];
+                }else{
+                    return row1[column] > row2[column];
+                }
+            }
+        }
+    }
+    return false;
+}
