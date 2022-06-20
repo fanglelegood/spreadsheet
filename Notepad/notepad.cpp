9b3bb72d10bb90bf06e07561f284d6982f4c7792
Fang Miao <fangmiaomin@163.com>
2022-06-20 23:25:05 +0800

    notepad学习完毕，复制还不能以后修正。

--- Notepad/notepad.cpp
+++ Notepad/notepad.cpp
@@ -1,11 +1,26 @@
 #include "notepad.h"
 #include "ui_notepad.h"
+#include <QMessageBox>
+#include <QTextStream>
+#include <QPrinter>
+#include <QPrintDialog>
+#include <QFontDialog>
 
 Notepad::Notepad(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Notepad)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+#if !defined(QT_PRINTSUPPORT_LIB) || !QT_CONFIG(printer)
+    ui->actionPrint->setEnabled(false);
+#endif
+
+#if !QT_CONFIG(clipboard)
+    ui->actionCut->setEnabled(false);
+    ui->actionCopy->setEnabled(false);
+    ui->actionPaste->setEnabled(false);
+#endif
 }
 
 Notepad::~Notepad()
@@ -18,5 +33,119 @@ void Notepad::on_actionNew_triggered()
 {
     currentFile.clear();
     ui->textEdit->setText(QString());
+}
+
+
+void Notepad::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Open the file");
+    QFile file(fileName);
+    currentFile = fileName;
+    if (!file.open(QIODevice::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this, "warning","connot open file" + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+
+void Notepad::on_actionSave_triggered()
+{
+    QString fileName;
+    if(currentFile.isEmpty()){
+        fileName = QFileDialog::getSaveFileName(this, "save");
+        currentFile = fileName;
+    } else {
+        fileName = currentFile;
+    }
+}
+
+
+void Notepad::on_actionSaveAs_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "save as");
+    QFile file(fileName);
+
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "warning", "cannot save file:" + file.errorString());
+        return;
+    }
+    currentFile = fileName;
+    setWindowTitle(fileName);
+    QTextStream out (&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+
+void Notepad::on_actionPrint_triggered()
+{
+#if defined(QT_PRINTSUPPORT_LIB) && QT_CONFIG(printer)
+    QPrinter printDev;
+#if QT_CONFIG(printdialog)
+    QPrintDialog dialog(&printDev, this);
+    if (dialog.exec() == QDialog::Rejected)
+        return;
+#endif // QT_CONFIG(printdialog)
+    ui->textEdit->print(&printDev);
+#endif // QT_CONFIG(printer)
+}
+
+
+
+void Notepad::on_actionExit_triggered()
+{
+    QCoreApplication::quit();
+}
+
+
+void Notepad::on_actionCopy_triggered()
+{
+#if QT_CONFIG(clipboard)
+    ui->textEdit->copy();
+#endif
+}
+
+
+void Notepad::on_actionCut_triggered()
+{
+#if QT_CONFIG(clipboard)
+    ui->textEdit->cut();
+#endif
+}
+
+
+void Notepad::on_actionpaste_triggered()
+{
+#if QT_CONFIG(clipboard)
+    ui->textEdit->paste();
+#endif
+}
+
+
+
+void Notepad::on_actionselectfont_triggered()
+{
+    bool fontSelected;
+    QFont font = QFontDialog::getFont(&fontSelected, this);
+    if (fontSelected)
+        ui->textEdit->setFont(font);
+}
+
+
+void Notepad::on_actionundo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+
+void Notepad::on_actionredo_triggered()
+{
+    ui->textEdit->redo();
 }
 
--- Notepad/notepad.h
+++ Notepad/notepad.h
@@ -2,6 +2,8 @@
 #define NOTEPAD_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QFileDialog>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Notepad; }
@@ -17,6 +19,28 @@ public:
 
 private slots:
     void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionSaveAs_triggered();
+
+    void on_actionPrint_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionpaste_triggered();
+
+    void on_actionselectfont_triggered();
+
+    void on_actionundo_triggered();
+
+    void on_actionredo_triggered();
 
 private:
     Ui::Notepad *ui;
